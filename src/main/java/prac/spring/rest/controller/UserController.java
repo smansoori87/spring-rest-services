915a70168ad97fea93bbbf5d5967b8c12d0dbf11
Suhail <Suhail@Suhail-PC>
2021-01-01 20:01:07 +0530

    commit.

--- src/main/java/prac/spring/rest/controller/UserController.java
+++ src/main/java/prac/spring/rest/controller/UserController.java
@@ -3,6 +3,7 @@ package prac.spring.rest.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +22,9 @@ public class UserController {
 		return "Hi-" + user;
 	}
 
-	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, 
-			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
-	public ResponseEntity<User> saveUser(@RequestBody User user) {
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<User> saveUser(@Validated @RequestBody User user) {
 		return new ResponseEntity<User>(user, HttpStatus.ACCEPTED);
 	}
 }
--- src/main/java/prac/spring/rest/model/User.java
+++ src/main/java/prac/spring/rest/model/User.java
@@ -1,12 +1,20 @@
 package prac.spring.rest.model;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class User {
+	@NotEmpty(message = "Mandatory field and cant be empty.")
 	private String id;
+
+	@NotEmpty(message = "Mandatory field and cant be empty.")
 	private String name;
+
+	@Pattern(regexp = "^\\d{2}-[a-zA-Z]{3}-\\d{4}$")
 	private String dob;
 }
